eca5db64e1cf8d3d8d9bc9efa3a0c73d5a7002dd
Kent Diamond <kent@skillsai.com>
2017-11-24 16:14:47 -0700

    initial commit

---
+++ source/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Component1 from './modules/component1/component1';
+
+ReactDOM.render(<Component1 width={ 1000 } height={ 800 } />, document.getElementById('app'));
---
+++ source/modules/component1/component1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Component1 extends React.Component{
+	static propsTypes = {
+		height: PropTypes.number,
+		width: PropTypes.number,
+	}
+	render() {
+		return (
+			<div>
+				Component1
+			</div>
+		)
+	}
+
+}
